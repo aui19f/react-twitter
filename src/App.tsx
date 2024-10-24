c8383a14cef9205ff4aabf346e60168f538b1ff3
aui19f <aui19f@gmail.com>
2024-10-24 17:35:01 +0900

    파이어베이스 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import Loading from "@/components/Loading";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "@/fbase";
 
 function App() {
-  return (
-    <>
-      <h1>APP</h1>
-      <Outlet />
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(false);
+  const init = async () => {
+    setIsLoading((prev) => !prev);
+    setTimeout(() => setIsLoading((prev) => !prev), 50000);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      console.log("IF");
+      // ...
+    } else {
+      console.log("ELSE");
+      // User is signed out
+      // ...
+    }
+  });
+
+  return <>{isLoading ? <Loading /> : <Outlet />}</>;
 }
 
 export default App;
